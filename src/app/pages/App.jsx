a98d7e540448a5f1bd0b72a1b0a3dff2340ca947
ShawnGitee <550513451@qq.com>
2023-02-21 21:44:14 +0800

    tag2

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -4,6 +4,9 @@ import { isAuthenticated } from '../../client/state/auth';
 
 import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-4MFGMV10G1');
 
 function App() {
   return isAuthenticated() ? <Client /> : <Auth />;
