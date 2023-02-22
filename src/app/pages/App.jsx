dc2c60a61555b1cc0b276bf5648f067ee27c76d3
ShawnGitee <550513451@qq.com>
2023-02-22 09:38:26 +0800

    react ga

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -6,7 +6,7 @@ import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
 import ReactGA from 'react-ga4';
 
-ReactGA.initialize('G-4MFGMV10G1');
+ReactGA.initialize(process.env.VITE_GOOGLE_ANALYTICS);
 
 function App() {
   return isAuthenticated() ? <Client /> : <Auth />;
